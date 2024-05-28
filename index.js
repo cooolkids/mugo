23dda44aba33cc0ed82881c76fc642dc301c6412
cooolkids <157353984+cooolkids@users.noreply.github.com>
2024-05-28 14:32:43 +0900

    real end

--- index.js
+++ index.js
@@ -17,4 +17,29 @@ $('.character ul li a').click(function(){
     $(this).addClass('on')
 })
 
+let clearId;
+function bannerRotate() {
+    $('.rotate').animate({ 'rotate': '+=5deg' }, '50', 'linear')
+}
+$('.quick').mouseenter(function () {
+    clearId = setInterval(bannerRotate, 5)
+    $('.rotate img').attr({ 'src': './images/quick-menu-line.png' })
+    $('.quicktxt').css({ 'background': '#d91114' })
+});
+$('.quick').mouseleave(function () {
+    $('.rotate').stop(1, 0)
+    $('.rotate img').attr({ 'src': './images/quick-menu-line-02.png' })
+    $('.quicktxt').css({ 'background': '#ed4043' })
+    clearInterval(clearId)
+});
 
+$('.tab-menu li').click(function (e) {
+    e.preventDefault()
+
+    let idx= $(this).index()
+    $('.tab-menu li').removeClass('on')
+    $(this).addClass('on')
+
+    $('.item-tab').removeClass('on')
+    $('.item-tab').eq(idx).addClass('on')
+})
